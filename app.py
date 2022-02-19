e8358c846bf09bead6a4f0c6e5108a891d2a545f
sterlingvm <sterlingvmiller1@gmail.com>
2022-02-19 16:30:49 -0600

    Updated flask app.py, weather data visualization now up on locally hosted webpage

--- app.py
+++ app.py
@@ -41,11 +41,59 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
     return(
-        '''
-        Welcome to the Climate ANalysis API!
-        Available Routes:
-        /api/v1.0/precipitation
-        /api/v1.0/stations
-        /api/v1.0/tobs
-        /api/v1.0/temp/start/end       
-        ''')
+        'Welcome to the Climate Analysis API! <br>'
+        '<br>'
+        'Available Routes:<br>'
+        '/api/v1.0/precipitation <br>'
+        '/api/v1.0/stations <br>'
+        '/api/v1.0/tobs <br>'
+        '/api/v1.0/temp/start/end <br>'
+        )
+# ^ ACCESS THIS ROUTE BY GOING TO http://127.0.0.1:5000/
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+      filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+# ^ ACCESS THIS ROUTE BY GOING TO http://127.0.0.1:5000/api/v1.0/precipitation
+
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+# ^ ACCESS THIS ROUTE BY GOING TO http://127.0.0.1:5000/api/v1.0/stations
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+# ^ ACCESS THIS ROUTE BY GOING TO http://127.0.0.1:5000/api/v1.0/tobs
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
+# ^ TO ACCESS DATA IN THIS ROUTE YOU HAVE TO IUNPUT A START AND END DATE
+# DO THIS BY INPUTTING YOUR DATES INTO THE LOCALHOST URL
+# EXAMPLE: http://127.0.0.1:5000/api/v1.0/temp/2017-06-01/2017-06-30
+
